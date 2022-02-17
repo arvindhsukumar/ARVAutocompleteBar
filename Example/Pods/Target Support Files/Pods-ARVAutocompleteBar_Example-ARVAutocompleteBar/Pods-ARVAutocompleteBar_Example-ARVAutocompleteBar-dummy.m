fbc6f3dae36bd06268f67e8e1338f4a387484630
removedporn <86824510+removedporn@users.noreply.github.com>
2022-02-17 17:07:42 +0800

    Delete Example directory

--- Example/Pods/Target Support Files/Pods-ARVAutocompleteBar_Example-ARVAutocompleteBar/Pods-ARVAutocompleteBar_Example-ARVAutocompleteBar-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ARVAutocompleteBar_Example_ARVAutocompleteBar : NSObject
-@end
-@implementation PodsDummy_Pods_ARVAutocompleteBar_Example_ARVAutocompleteBar
-@end
--- Example/Pods/Target Support Files/Pods-ARVAutocompleteBar_Tests-ARVAutocompleteBar/Pods-ARVAutocompleteBar_Tests-ARVAutocompleteBar-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ARVAutocompleteBar_Tests_ARVAutocompleteBar : NSObject
-@end
-@implementation PodsDummy_Pods_ARVAutocompleteBar_Tests_ARVAutocompleteBar
-@end
